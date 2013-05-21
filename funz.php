7a04e505f46105ac05cd65cfdef1bae71fae559c
filippo <filippo.giomi@gmail.com>
2013-05-21 09:39:08 +0200

    first commit

---
+++ funz.php
@@ -0,0 +1,156 @@
+<?php
+include("../includes/config.php");
+$host=$config['MasterServer']['servername'];
+$user=$config['MasterServer']['username'];
+$pass=$config['MasterServer']['password'];
+$nomedb=$config['Database']['dbname'];
+function connetti(&$db) {
+	global $host, $user, $pass, $nomedb;
+	$db = new mysqli($host, $user, $pass, $nomedb);
+	if (mysqli_connect_errno()) {
+		echo 'Connessione fallita';
+		exit ;
+	}
+}
+
+function seleziona(&$db, $query) {
+	return $db -> query($query);
+}
+
+function get_num_rows($result) {
+	return $result -> num_rows;
+}
+
+function scorri_record($result) {
+	return $result -> fetch_assoc();
+}
+
+function scrivilistapost($mese, $anno, $mese1, $anno1) {
+	connetti($db);
+	$query = "SELECT username, count(*) post FROM vb_post WHERE dateline >= unix_timestamp(DATE('" . $anno . "-" . $mese . "-01')) and dateline < unix_timestamp(DATE('" . $anno1 . "-" . ($mese1) . "-01')) and username<>'' and userid<>770 group by username order by post desc limit 10";
+	$result = seleziona($db, $query);
+	$n_righe = get_num_rows($result);
+	if ($n_righe > 0) {
+		echo '<table>';
+		echo '<tr><th>Username</th><th>Post scritti</th></tr>';
+		for ($i = 0; $i < $n_righe; $i++) {
+			$riga = scorri_record($result);
+			echo '<tr><td>' . $riga['username'] . '</td><td>' . $riga['post'] . '</td></tr>';
+		}
+		echo '</table>';
+	}
+}
+
+function scrivilistathread($mese, $anno, $mese1, $anno1) {
+	connetti($db);
+	$query = "SELECT postusername, count(*) thread FROM `vb_thread` WHERE dateline >= unix_timestamp(DATE('".$anno."-".$mese."-01')) and dateline < unix_timestamp(DATE('" . $anno1 . "-" . ($mese1) . "-01')) and postusername<>'' and postuserid<>770 group by postusername order by thread desc limit 10";
+	$result = seleziona($db, $query);
+	$n_righe = get_num_rows($result);
+	if ($n_righe > 0) {
+		echo '<table>';
+		echo '<tr><th>Username</th><th>Thread aperti</th></tr>';
+		for ($i = 0; $i < $n_righe; $i++) {
+			$riga = scorri_record($result);
+			echo '<tr><td>' . $riga['postusername'] . '</td><td>' . $riga['thread'] . '</td></tr>';
+		}
+		echo '</table>';
+	}
+}
+
+function scrividiscussioni($mese, $anno, $mese1, $anno1) {
+	connetti($db);
+	$query="select title, postusername, replycount from vb_thread where dateline >= unix_timestamp(DATE('".$anno."-".$mese."-01')) and dateline < unix_timestamp(DATE('" . $anno1 . "-" . ($mese1) . "-01')) and postusername<>'' and postuserid<>770 order by replycount desc limit 10";
+	$result = seleziona($db, $query);
+	$n_righe = get_num_rows($result);
+	if ($n_righe > 0) {
+		echo '<table>';
+		echo '<tr><th>Username</th><th>Thread</th><th>Reply</th>';
+		for ($i = 0; $i < $n_righe; $i++) {
+			$riga = scorri_record($result);
+			echo '<tr><td>' . $riga['postusername'] . '</td><td>' . trunc($riga['title'],50) . '</td><td>' . $riga['replycount'] . '</td></tr>';
+		}
+		echo '</table>';
+	}
+}
+
+function scrivimipiace($mese, $anno, $mese1, $anno1) {
+	connetti($db);
+	$query="SELECT username, count(*) n FROM vb_vbseo_likes left join vb_user on l_dest_userid=userid where l_dateline >= unix_timestamp(DATE('".$anno."-".$mese."-01')) and l_dateline < unix_timestamp(DATE('" . $anno1 . "-" . ($mese1) . "-01')) and username<>'' and userid<>770 group by username order by n desc limit 10";
+	$result = seleziona($db, $query);
+	$n_righe = get_num_rows($result);
+	if ($n_righe > 0) {
+		echo '<table>';
+		echo '<tr><th>Username</th><th>"Mi piace" ricevuti</th>';
+		for ($i = 0; $i < $n_righe; $i++) {
+			$riga = scorri_record($result);
+			echo '<tr><td>' . $riga['username'] . '</td><td>' . trunc($riga['n'],50) . '</td></tr>';
+		}
+		echo '</table>';
+	}
+}
+
+function periodi($mese,$anno,$tot) {
+	connetti($db);
+	$query = "select distinct year(from_unixtime(`dateline`)) a from vb_post order by a desc";
+	$result = seleziona($db, $query);
+	$n_righe = get_num_rows($result);
+	if ($n_righe > 0) {
+		echo '<div id="sidetreecontrol" style="padding-top:10px"> <a href="">Comprimi</a> | <a href="">Espandi</a> </div>
+		Anni
+		<ul class="treeview" id="tree">';
+		for ($i = 0; $i < $n_righe; $i++) {
+			$anni = scorri_record($result);
+			$query = "select distinct month(from_unixtime(`dateline`)) m from vb_post where year(from_unixtime(`dateline`)) = ".$anni['a']." order by m asc";
+			$result2 = seleziona($db, $query);
+			$n_righe2 = get_num_rows($result2);
+			if ($n_righe2>0) {
+				echo '<li class="expandable"><div class="hitarea expandable-hitarea"></div> ';
+				if ((int)$anni['a']==$anno)
+					echo '<a href="index.php?mese=0&anno='.$anni['a'].'"><b>'.$anni['a'].'</b></a>';
+				else 
+					echo '<a href="index.php?mese=0&anno='.$anni['a'].'">'.$anni['a'].'</a>';
+				echo '<ul style="display: none;">';
+				for ($z=0; $z<$n_righe2; $z++) {
+					$mesi=scorri_record($result2);
+					if ($tot==0 && (((int)$mesi['m']==$mese) && ((int)$anni['a']==$anno))){
+						echo '<li><a href="index.php?mese='.$mesi['m'].'&anno='.$anni['a'].'"><b>'.mese_desc($mesi['m']).'</b></a></li>';
+						}
+					else {
+						echo '<li><a href="index.php?mese='.$mesi['m'].'&anno='.$anni['a'].'">'.mese_desc($mesi['m']).'</a></li>';
+						}
+				}
+				echo '</ul></li>';
+			}
+		}
+		echo '</ul>';
+	}
+}
+
+function mese_desc($i) {
+	switch ($i) {
+		case 1: return 'Gennaio';    break;
+		case 2: return 'Febbraio';   break;
+		case 3: return 'Marzo';      break;
+		case 4: return 'Aprile';     break;
+		case 5: return 'Maggio';     break;
+		case 6: return 'Giugno';     break;
+		case 7: return 'Luglio';     break;
+		case 8: return 'Agosto';     break;
+		case 9: return 'Settembre';  break;
+		case 10: return 'Ottobre';   break;
+		case 11: return 'Novembre';  break;
+		case 12: return 'Dicembre';  break;
+		
+		default: return '---';       break;
+	}
+}
+
+function trunc($s, $l) {
+	if (strlen($s) > $l) 
+		$r=substr($s,0,($l-3)).'...'; 
+	else
+		$r=$s;
+	return $r;
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,92 @@
+<?php
+include('funz.php');
+if ((isset($_GET['mese'])) && (isset($_GET['anno']))) {
+	$mese=(int)$_GET['mese'];
+	$anno=(int)$_GET['anno'];
+	if (($mese<0)||($mese>12)||($anno<2010)||($anno>Date("Y"))) {
+		echo '<script>alert("Lascia stare...");location.href = "/fwstats/";</script>';
+		exit;
+	}
+	if (($anno==date("Y")) & ($mese>date("n"))) {
+		echo '<script>alert("Eh, mica possiamo predire il futuro...");location.href = "/fwstats/";</script>';
+		exit;
+	}
+}	
+else { //ultimo mese
+	$mese=date("n");
+	$anno=date("Y");
+}
+
+switch ($mese) {
+	case 0: //tutto l'anno
+		$mese=1;
+		$mese1=1;
+		$anno1=$anno+1;
+		$tot=1;
+		break;
+	case 12: //dicembre
+		$mese1=1;
+		$anno1=$anno+1;
+		$tot=0;
+		break;
+	default:
+		$mese1=$mese+1;
+		$anno1=$anno;
+		$tot=0;
+		break;
+}
+?>
+
+<html>
+	<title>Statistiche ForgottenWorld</title>
+	<link rel="stylesheet" type="text/css" href="include/style.css" />
+	<link rel="stylesheet" href="include/jquery.treeview.css" />
+	<script src="include/jquery.js" type="text/javascript"></script>
+	<script src="include/jquery.treeview.js" type="text/javascript"></script>
+	
+	<script type="text/javascript">
+		$(function() {
+			$("#tree").treeview({
+				collapsed: true,
+				animated: "fast",
+				control:"#sidetreecontrol",
+				prerendered: true,
+				persist: "location"
+			});
+		});
+	</script>
+	
+	<table width ='100%' cellpadding = '10px'>
+		<tr>
+			<td align="center" colspan="4">
+				<b>Statistiche <?php if ($tot==0)
+						echo mese_desc($mese).' ';
+					echo $anno;?></b>
+			</td>
+		</tr>
+		<tr>
+			<td align='left' valign="top" rowspan="2">
+				<?php periodi($mese, $anno, $tot); ?>
+			</td>
+			<td align='center' id='elenco'>
+				<?php scrivilistapost($mese, $anno, $mese1, $anno1); ?>
+			</td>
+			<td align='center' id='elenco'>
+				<?php scrivilistathread($mese, $anno, $mese1, $anno1); ?>
+			</td>
+			<td align='center' id='elenco'>
+				<?php scrivimipiace($mese, $anno, $mese1, $anno1); ?>
+			</td>
+		</tr>
+		<tr>
+			<td align="center" colspan="3" id='elenco'>
+				<?php scrividiscussioni($mese, $anno, $mese1, $anno1); ?>
+			</td>
+		</tr>
+		<tr>
+			<td align="right" colspan="4" id='footer'>
+				By <a href="http://www.forgottenworld.it/members/doc.html">Doc</a> - <a href="https://github.com/doc90/fwstats">GitHub</a>
+			</td>
+		</tr>
+	</table>
+</html>
