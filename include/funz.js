9c6cd7f49b2d716e0159bce47b5fc7670646e6ec
filippo <filippo.giomi@gmail.com>
2013-05-22 14:17:32 +0200

    aggiunto fadein e fadeout

--- include/funz.js
+++ include/funz.js
@@ -5,44 +5,59 @@ function carica(mese,anno) {
 				else
 					$('#p').html(x.anno);
 			});
+			$('#toppost').fadeOut();
 			$.getJSON("data.php?t=1&mese="+mese+"&anno="+anno, function(jsonData){
-					t='<table id="elenco"><tr><th>Username</th><th>Post scritti</th></tr>';
-					$.each(jsonData, function(i, data) {
-									t+='<tr><td>'+data.username+'</td><td>'+data.post+'</td></tr>';									
-					});
-					t+='</table>';
-					$('#toppost').html(t);
+				t='<table id="elenco"><tr><th>Username</th><th>Post scritti</th></tr>';
+				$.each(jsonData, function(i, data) {
+								t+='<tr><td>'+data.username+'</td><td>'+data.post+'</td></tr>';									
+				});
+				t+='</table>';
+				$('#toppost').html(t);
 			});
+			$('#toppost').fadeIn();
+			$('#topthread').fadeOut();
 			$.getJSON("data.php?t=2&mese="+mese+"&anno="+anno, function(jsonData){
-					t='<table id="elenco"><tr><th>Username</th><th>Tread aperti</th></tr>';
-					$.each(jsonData, function(i, data) {
-									t+='<tr><td>'+data.username+'</td><td>'+data.thread+'</td></tr>';									
-					});
-					t+='</table>';
-					$('#topthread').html(t);
+				t='<table id="elenco"><tr><th>Username</th><th>Tread aperti</th></tr>';
+				$.each(jsonData, function(i, data) {
+								t+='<tr><td>'+data.username+'</td><td>'+data.thread+'</td></tr>';									
+				});
+				t+='</table>';
+				$('#topthread').html(t);
 			});
-			$.getJSON("data.php?t=3&mese="+mese+"&anno="+anno, function(jsonData){
+			$('#topthread').fadeIn();
+			$('#topmipiace').fadeOut();
+			try{
+				$.getJSON("data.php?t=3&mese="+mese+"&anno="+anno, function(jsonData){
 					t='<table id="elenco"><tr><th>Username</th><th>"Mi piace" ricevuti</th></tr>';
 					$.each(jsonData, function(i, data) {
 									t+='<tr><td>'+data.username+'</td><td>'+data.mipiace+'</td></tr>';									
 					});
 					t+='</table>';
 					$('#topmipiace').html(t);
-			});
+				});
+			}
+			catch(e){
+				$('#topmipiace').html('<b>Le statistiche "mi piace"<br />sono disponibili a partire da maggio 2013');
+				}
+			$('#topmipiace').fadeIn();
+			$('#topreply').fadeOut();
 			$.getJSON("data.php?t=4&mese="+mese+"&anno="+anno, function(jsonData){
-					t='<table id="elenco"><tr><th>Username</th><th>Trhead</th><th>Reply</th></tr>';
-					$.each(jsonData, function(i, data) {
-									t+='<tr><td>'+data.username+'</td><td>'+data.title+'</td><td>'+data.reply+'</td></tr>';									
-					});
-					t+='</table>';
-					$('#topreply').html(t);
+				t='<table id="elenco"><tr><th>Username</th><th>Trhead</th><th>Reply</th></tr>';
+				$.each(jsonData, function(i, data) {
+								t+='<tr><td>'+data.username+'</td><td>'+data.title+'</td><td>'+data.reply+'</td></tr>';									
+				});
+				t+='</table>';
+				$('#topreply').html(t);
 			});
+			$('#topreply').fadeIn();
+			$('#topvarie').fadeOut();
 			$.getJSON("data.php?t=5&mese="+mese+"&anno="+anno, function(jsonData){
-					t='<b>Varie</b><table id="elenco">';
-					$.each(jsonData, function(i, data) {
-									t+='<tr><td>'+data.desc+'</td><td>'+data.n+'</td></tr>';									
-					});
-					t+='</table>';
-					$('#topvarie').html(t);
+				t='<b>Varie</b><table id="elenco">';
+				$.each(jsonData, function(i, data) {
+								t+='<tr><td>'+data.desc+'</td><td>'+data.n+'</td></tr>';									
+				});
+				t+='</table>';
+				$('#topvarie').html(t);
 			});
+			$('#topvarie').fadeIn();
 		}
